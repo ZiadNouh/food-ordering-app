998fde0109b4cb16724d13d281f490a6b574de90
ziad <ziad.nouh111@gmail.com>
2025-10-10 21:58:54 +0300

    feat: implement MenuPage component to display products by category

--- src/server/db/products.ts
+++ src/server/db/products.ts
@@ -17,3 +17,21 @@ export const getBestSellers = cache(
   ["best-sellers"],
   { revalidate: 3600 }
 );
+
+export const getProductsByCategory = cache(
+  () => {
+    const categories = db.category.findMany({
+      include: {
+        products: {
+          include: {
+            sizes: true,
+            extras: true,
+          },
+        },
+      },
+    });
+    return categories;
+  },
+  ["products-by-category"],
+  { revalidate: 3600 }
+);
