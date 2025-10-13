da2bafa6c198cdba3487dfeee4b26afafe6f3032
ziad <ziad.nouh111@gmail.com>
2025-10-13 14:29:00 +0300

    feat: moved the files to [locale] for i18n

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,6 @@
-import BestSellers from "@/app/_components/BestSellers";
-import Hero from "@/app/_components/Hero";
-import AboutPage from "@/app/about/page";
+import BestSellers from "@/app/[locale]/_components/BestSellers";
+import Hero from "@/app/[locale]/_components/Hero";
+import AboutPage from "@/app/[locale]/about/page";
 import About from "@/components/about";
 import Contact from "@/components/contact";
 
